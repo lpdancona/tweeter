e8a81e0e3323767a804a1ca82f672fb510405953
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-02-04 12:57:56 -0800

    fixed final version

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   console.log("Hello Client");
   $("textarea").keyup(function () {
     const textLength = $(this).val().length;
-    $(".counter").val(140 - textLength);
+    $("output").val(140 - textLength);
     if (textLength > 140) {
-      $(".counter").css({ color: "red" });
+      $("output").css({ color: "red" });
     } else {
-      $(".counter").css({ color: "inherit" });
+      $("output").css({ color: "inherit" });
     }
   });
 });
